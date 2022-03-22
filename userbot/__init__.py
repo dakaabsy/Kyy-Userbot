1963051405ae27042e490398897f2f72e7dc21ed
dakaabsy <102158323+dakaabsy@users.noreply.github.com>
2022-03-22 20:26:32 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -179,7 +179,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/muhammadrizky16/Kyy-Userbot")
+    "https://github.com/dakaabsy/Kyy-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Kyy-Userbot")
 
